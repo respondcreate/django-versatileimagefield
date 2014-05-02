70df023f9dda87350d8ed9126fc42c15d17a96c2
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2014-05-02 11:17:29 -0400

    Adding setup.py, LICENSE, and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-versatilefield',
+    packages=find_packages(),
+    version='0.1',
+    author=u'Jonathan Ellenberger',
+    author_email='jonathan_ellenberger@wgbh.org',
+    url='http://github.com/WGBH/django-versatileimagefield/',
+    license='MIT License, see LICENSE',
+    description="A drop-in replacement for django's ImageField that provides "
+                "a flexible, intuitive and easily-extensible interface for "
+                "quickly creating new images from the one assigned to your "
+                "field.",
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    install_requires=[
+        'Pillow>=2.4.0',
+    ],
+    package_data={
+        'versatileimagefield': [
+            'static/versatileimagefield/css/*.css',
+            'static/versatileimagefield/js/*.js',
+            'static/versatileimagefield/images/*.png'
+        ]
+    },
+    classifiers=[
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Multimedia :: Graphics :: Presentation'
+    ]
+)
