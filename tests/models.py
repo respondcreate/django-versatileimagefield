de1dd43b3dd8c17a7050ab3d47baef188f13c7e2
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2014-12-23 17:07:04 -0500

    Adding a tests-specific app
    Includes:
    * Models + fixture data
    * Settings
    * Media used in fixture

---
+++ tests/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from versatileimagefield.fields import VersatileImageField, PPOIField
+
+
+class VersatileImageTestModel(models.Model):
+    """A model for testing VersatileImageFields"""
+    img_type = models.CharField(
+        max_length=5,
+        unique=True
+    )
+    image = VersatileImageField(
+        upload_to='',
+        ppoi_field='ppoi'
+    )
+    ppoi = PPOIField()
---
+++ tests/test_settings.py
@@ -0,0 +1,71 @@
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
+SECRET_KEY = 'fake-key'
+INSTALLED_APPS = [
+    "tests",
+    "versatileimagefield"
+]
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+    }
+}
+
+VERSATILEIMAGEFIELD_SETTINGS = {
+    # The amount of time, in seconds, that references to created images
+    # should be stored in the cache. Defaults to `2592000` (30 days)
+    'cache_length': 2592000,
+    # The name of the cache you'd like `django-versatileimagefield` to use.
+    # Defaults to 'versatileimagefield_cache'. If no cache exists with the name
+    # provided, the 'default' cache will be used instead.
+    'cache_name': 'versatileimagefield_cache',
+    # The save quality of modified JPEG images. More info here:
+    # http://pillow.readthedocs.org/en/latest/handbook/image-file-formats.html#jpeg
+    # Defaults to 70
+    'jpeg_resize_quality': 70,
+    # A path on disc to an image that will be used as a 'placeholder'
+    # for non-existant images.
+    # If 'global_placeholder_image' is unset, the excellent, free-to-use
+    # http://placehold.it service will be used instead.
+    # The name of the top-level folder within storage classes to save all
+    # sized images. Defaults to '__sized__'
+    'sized_directory_name': '__sized__',
+    # The name of the directory to save all filtered images within.
+    # Defaults to '__filtered__':
+    'filtered_directory_name': '__filtered__',
+    # Whether or not to create new images on-the-fly. Set this to `False` for
+    # speedy performance but don't forget to 'pre-warm' to ensure they're
+    # created and available at the appropriate URL.
+    'create_images_on_demand': False
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'versatileimagefield_testing_cache',
+    }
+}
+
+MEDIA_URL = '/media/'
+
+VERSATILEIMAGEFIELD_RENDITION_KEY_SETS = {
+    'test_set': (
+        ('test_thumb', 'thumbnail__100x100'),
+        ('test_crop', 'crop__100x100'),
+        ('test_invert', 'filters__invert__url'),
+        ('test_invert_thumb', 'filters__invert__thumbnail__100x100'),
+        ('test_invert_crop', 'filters__invert__crop__100x100'),
+    ),
+}
