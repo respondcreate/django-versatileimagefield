f2c592fd356d3fe2032c8e0f7621612362d1585f
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2015-04-27 14:13:31 -0400

    Adding django >= 1.7.x app config

---
+++ versatileimagefield/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class VersatileImageFieldConfig(AppConfig):
+    """The Django app config for django-versatileimagefield."""
+    name = 'versatileimagefield'
+    verbose_name = "VersatileImageField"
