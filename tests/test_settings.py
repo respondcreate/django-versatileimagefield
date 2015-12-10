f59fb0e96ad4f7f034251b44a73e74dfa1c56f69
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2015-12-10 10:18:57 -0500

    Adding TEMPLATES to test settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -92,3 +92,21 @@ VERSATILEIMAGEFIELD_RENDITION_KEY_SETS = {
 
 ROOT_URLCONF = 'tests.urls'
 DEBUG = True
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(PROJECT_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
