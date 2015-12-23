9dc1dcf5e70b510334c1aa095a812f518a759488
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2015-12-23 09:59:52 -0500

    Making autodiscover django app config friendly

--- versatileimagefield/apps.py
+++ versatileimagefield/apps.py
@@ -5,3 +5,7 @@ class VersatileImageFieldConfig(AppConfig):
     """The Django app config for django-versatileimagefield."""
     name = 'versatileimagefield'
     verbose_name = "VersatileImageField"
+
+    def ready(self):
+        from .registry import autodiscover
+        autodiscover()
