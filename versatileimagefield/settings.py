b557b499dc47a781b6900fd81de2ba132559e128
Jonathan Ellenberger <jonathan_ellenberger@wgbh.org>
2016-04-10 07:15:17 -0400

    Re-adding Django 1.5.x & 1.6.x #36
    Enabled by defining django.utils.module_loading.import_string for
    pre-Django 1.7.x

--- versatileimagefield/settings.py
+++ versatileimagefield/settings.py
@@ -1,6 +1,32 @@
 from __future__ import unicode_literals
+try:
+    from django.utils.module_loading import import_string
+except ImportError:
+    import sys
+    from importlib import import_module
+    from django.utils import six
 
-from django.utils.module_loading import import_string
+    def import_string(dotted_path):
+        """
+        Import a dotted module path and return the attribute/class designated
+        by the last name in the path. Raise ImportError if the import failed.
+
+        Lifted from Django>=1.7.x
+        """
+        try:
+            module_path, class_name = dotted_path.rsplit('.', 1)
+        except ValueError:
+            msg = "%s doesn't look like a module path" % dotted_path
+            six.reraise(ImportError, ImportError(msg), sys.exc_info()[2])
+
+        module = import_module(module_path)
+
+        try:
+            return getattr(module, class_name)
+        except AttributeError:
+            msg = 'Module "%s" does not define a "%s" attribute/class' % (
+                module_path, class_name)
+            six.reraise(ImportError, ImportError(msg), sys.exc_info()[2])
 
 from django.conf import settings
 from django.core.cache import (
