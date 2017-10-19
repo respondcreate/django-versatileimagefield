5c1a0ff7e6ec16a8ea1e87396bb9eaa85921a941
Matthias Kestenholz <mk@feinheit.ch>
2017-10-19 21:46:57 +0200

    Fix #112: Exclude the top-level "tests" folder when building a package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-versatileimagefield',
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     version='1.7.1',
     author=u'Jonathan Ellenberger',
     author_email='jonathan_ellenberger@wgbh.org',
