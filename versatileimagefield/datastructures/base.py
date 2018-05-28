349417922ca2771e4d12a4a70907d03eb3b1b5f2
jxltom <jxltom@users.noreply.github.com>
2018-05-28 13:14:19 +0800

    Update BytesIO size instead of None when initialize InMemoryUploadedFile

--- versatileimagefield/datastructures/base.py
+++ versatileimagefield/datastructures/base.py
@@ -165,7 +165,7 @@ class ProcessedImage(object):
             None,
             'foo.%s' % file_ext,
             mime_type,
-            None,
+            imagefile.getbuffer().nbytes,
             None
         )
         file_to_save.seek(0)
